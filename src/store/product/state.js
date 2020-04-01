7b5fda0c2bf12566f5830125e897ef66c9908794
tunchamroeun <chamroeuntun25@outlook.com>
2020-04-01 17:02:44 +0700

    lyput.app

--- src/store/product/state.js
+++ src/store/product/state.js
@@ -2,7 +2,7 @@ import {LocalStorage} from "quasar";
 
 export default function () {
   return {
-    products: LocalStorage.getItem('products')||[],
+    products: LocalStorage.getItem('products') || [{'id':0,'name':'No result'}],
     product_count: null,
   }
 }
